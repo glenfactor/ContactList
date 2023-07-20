e7c7548f466e166ad8229460095b3cff55e50f2e
mloisking <126358439+mloisking@users.noreply.github.com>
2023-07-20 19:56:29 -0400

    added new codes to API

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ContactRow from "./ContactRow";
 import {useEffect} from 'react'
+
 const dummyContacts = [
   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
   { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
@@ -13,13 +14,15 @@ export default function ContactList() {
   useEffect(() => {
     async function fetchContacts() {
       try {
-        // your fetch logic will go here
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchContacts()
-  }, []);
+        const response=await fetch('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users')
+            const result=await response.json();
+            setContacts(result);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        fetchContacts();
+      }, []);
   return (
     <table>
       <thead>
