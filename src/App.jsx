254258634cd4bf3254e7232707a1c7d2857c2f5b
mloisking <126358439+mloisking@users.noreply.github.com>
2023-07-21 12:01:43 -0400

    added new codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-
+import "./App.css";
 import React from 'react';
 import ContactList from './components/ContactList';
 
-export default function App() {
-  return (
-    <>
-      <ContactList />
-    </>
-  );
-};
 
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+  ContactList (setSelectedContactId={setSelectedContactId} )
+ContactList = (props) => {
+    return <div className= {props.contact}></div>
+  }
+    <ContactList>
+      <tr>
+        <td>
+          {Contact.name}
+          {Contact.email}
+          {contact.phone}
+        </td>
+        </tr>
+    </ContactList>
+
+  
+}
  
+// create a second component folder with files contact list and contact row
+//Create two component codes for contact list and contact row
 
